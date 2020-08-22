cfe520e39e5efd7ce1b2db12408e192417df6066
sakamotoyuya <freetairu@yahoo.co.jp>
2020-08-23 08:05:15 +0900

    phpunitでテスト環境を作成

---
+++ tests/HumanTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+final class SakamotoTest extends TestCase{
+    public function testA(){
+        $obj = new Human();
+        $this->assertSame("こんにちわ",$obj->helloString());
+    }
+    public function testB(){
+        $obj = new Human();
+        // $this->assertSame("おーまいっが！",$obj->goodnightString());
+        $this->assertSame("おやすみ",$obj->goodnightString());
+    }
+    public function testD(){
+        $obj = new Human();
+        $this->assertSame("おやすみ",$obj->goodnightString());
+    }
+}
